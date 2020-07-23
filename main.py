926eea1a57bc64f52036e2d44a4283db86d26d8f
Milena Gj Perusheska <milena_perusevska@hotmail.com>
2020-07-23 09:48:53 +0200

    fix reading

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
 import pandas as pd
 
 
-if __name__ == "__main__":
-
+def read_data():
     data_path = './Files/Tables.csv'
-    column_names = ["Table_Name","Column_Name","Data_Type {nullable, unique}","Anonymization_Type {None, Omit, Randomize, Default value, Encode}"]
-    reader = pd.read_csv(data_path, names=column_names, skiprows=1, na_values="?", sep=",", skipinitialspace=True)
+    column_names = ["Table_Name", "Column_Name", "Data_Type {nullable, unique}",
+                    "Anonymization_Type {None, Omit, Randomize, Default value, Encode}"]
+    reader = pd.read_csv(data_path, encoding="windows-1252", names=column_names, skiprows=1, na_values="?", sep=",",
+                         skipinitialspace=True)
 
-    for i in range(0,(len(reader) - 1) ):
+    for i in range(0, (len(reader) - 1)):
         table_path = './Files/' + reader['Table_Name'][i] + '.csv'
-        table_column_names = reader['Table_Columns_Name']
-        table_reader = pd.read_csv(table_path, names=table_column_names, skiprows=1, na_values="?", sep=",", skipinitialspace=True)
+        table_column_names = reader['Column_Name']
+        table_reader = pd.read_csv(table_path, encoding="windows-1252", names=table_column_names, skiprows=1,
+                                   na_values="?", sep=",", skipinitialspace=True)
 
         # this is the data that needs to be protected
         protectedList = table_reader[reader["Column_Name"][i]]
+        return protectedList
 
 
-        # call functions for anonymization
+if __name__ == "__main__":
+    protectedList = read_data()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # call functions for anonymization

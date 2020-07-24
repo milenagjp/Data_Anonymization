e6ab26b53ac9f096509c93089af96708d432e2e4
Klimentina Dzeparoska <klimentina.dzeparoska@miraclemill.com>
2020-07-24 10:20:16 +0200

    added switch function

--- DataAnonimyzation.py
+++ DataAnonimyzation.py
@@ -2,10 +2,22 @@ import pandas as pd
 import random as rd
 
 
+def none_function(table_data, column_data, column_name):
+    return pd.DataFrame(table_data)
+
+
+def omit(table_data, column_data, column_name):
+    return pd.DataFrame(table_data)
+
+
+def null_function(table_data, column_data, column_name):
+    print('null function')
+    return pd.DataFrame(table_data)
+
+
 def default(df, column, column_name):
     dictionary = {value: i for i, value in enumerate(column.unique())}
     df[column_name].replace(dictionary, inplace=True)
-    print(df)
     return df
 
 
@@ -16,7 +28,7 @@ def random(df, column, column_name):
     return df
 
 
-def randomPseudonym(df, column, column_name):
+def random_pseudonym(df, column, column_name):
     dictionary = {}
     for value in column:
         sample = rd.sample(range(1, len(column) * 10), len(column))
@@ -26,8 +38,24 @@ def randomPseudonym(df, column, column_name):
     return df
 
 
+def random_from_set(table_data, column_data, column_name):
+    return pd.DataFrame(table_data)
+
+
+def switch_function(function_name, column_data, column_name, table_data):
+    switcher = {
+        'None': none_function(table_data, column_data, column_name),
+        'Omit': omit(table_data, column_data, column_name),
+        'Null': null_function(table_data, column_data, column_name),
+        'Default': default(table_data, column_data, column_name),
+        'Random': random(table_data, column_data, column_name),
+        'RandomFromSet': random_from_set(table_data, column_data, column_name),
+        'RandomPseudonym': random_pseudonym(table_data, column_data, column_name)
+    }
+    return switcher.get(function_name, "Invalid function name")
+
+
 if __name__ == "__main__":
-    i = 0
     data_path = './Files/Tables.csv'
     column_names = ["Table_Name", "Column_Name", "Data_Type {nullable, unique}",
                     "Anonymization_Type {None, Omit, Randomize, Default value, Encode}", 'Table_Column_Name']
@@ -45,18 +73,21 @@ if __name__ == "__main__":
         table_reader = pd.read_csv(table_path, encoding="windows-1252", names=table_column_names, skiprows=1,
                                    na_values="?", sep=",", skipinitialspace=True)
         # this is the data from the column that needs to be protected
-        protectedList = table_reader[reader["Column_Name"][i]]
-        # print(reader['Table_Name'][i])
-        # print(protectedList)
+        protected_column_name = reader["Column_Name"][i]
+        protectedList = table_reader[protected_column_name]
+
+        # see which function should be used for anonymization
+        anonymization_type = reader['Anonymization_Type {None, Omit, Randomize, Default value, Encode}'][i]
+
         # call functions for anonymization
-        if reader['Table_Name'][i] == 'Purchasing.Vendor':
-            anonymized = default(table_reader, protectedList, reader["Column_Name"][i])
-            anonymized.to_csv(table_path, index=False)
+        anonymized = switch_function(anonymization_type, protectedList, protected_column_name, table_reader)
+        
+        # new table path
+        new_table_path = table_path[:-4] + '-anonymized.csv'
 
-        if reader['Table_Name'][i] == 'Sales.CreditCard' or reader['Table_Name'][i] == 'Purchasing.PurchaseOrderHeader':
-            anonymized = random(table_reader, protectedList, reader["Column_Name"][i])
-            anonymized.to_csv(table_path, index=False)
+        # create new file
+        f = open(new_table_path, "w+")
+        f.close()
 
-        if reader['Table_Name'][i] == 'Production.Product' or reader['Table_Name'][i] == 'HumanResources.Employee':
-            anonymized = randomPseudonym(table_reader, protectedList, reader["Column_Name"][i])
-            anonymized.to_csv(table_path, index=False)
+        # write to file
+        anonymized.to_csv(new_table_path, index=False)
